05b33c9aaa0d7eb2eb50c1e889e4b603233a6ba1
Ludovic Lamarche <snigle64@gmail.com>
2018-03-06 18:43:29 +0100

    fix

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+import React from "react";
 import { AppRegistry } from "react-native";
 import App from "./lib/Router";
 AppRegistry.registerComponent("freeconomy", () => App);
 
+console.log("load web ", window.document);
  if (window.document) {
+   console.log("log react-native-web");
      AppRegistry.runApplication("freeconomy", {
          initialProps : {},
          rootTag : document.getElementById("react-root"),
