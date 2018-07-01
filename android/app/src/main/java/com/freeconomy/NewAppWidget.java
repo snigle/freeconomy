775b208b3f66da5375303542ce3b2ff5afb2547c
Ludovic Lamarche <snigle64@gmail.com>
2018-07-01 13:56:56 +0200

    fix null pointer exception in google tests

--- android/app/src/main/java/com/freeconomy/NewAppWidget.java
+++ android/app/src/main/java/com/freeconomy/NewAppWidget.java
@@ -23,7 +23,6 @@ import com.oblador.vectoricons.VectorIconsModule;
 import java.io.IOException;
 import java.text.DecimalFormat;
 
-
 /**
  * Implementation of App Widget functionality.
  */
@@ -31,8 +30,8 @@ public class NewAppWidget extends AppWidgetProvider {
 
     static String TAG = "WalletWidget";
 
-    public static final String WIDGET_IDS_KEY ="mywidgetproviderwidgetids";
-    public static final String WIDGET_DATA_KEY ="mywidgetproviderwidgetdata";
+    public static final String WIDGET_IDS_KEY = "mywidgetproviderwidgetids";
+    public static final String WIDGET_DATA_KEY = "mywidgetproviderwidgetdata";
 
     static class ID {
         public int WalletName;
@@ -40,38 +39,44 @@ public class NewAppWidget extends AppWidgetProvider {
         public int Wallet;
         public int WalletBox;
     }
-    public static ID[] ids = new ID[]{
-            new ID(){{
-                WalletName = R.id.wallet_name_1;
-                WalletPrice = R.id.wallet_price_1;
-                Wallet = R.id.wallet_1;
-                WalletBox = R.id.wallet_box_1;
-            }},
-            new ID(){{
-                WalletName = R.id.wallet_name_2;
-                WalletPrice = R.id.wallet_price_2;
-                Wallet = R.id.wallet_2;
-                WalletBox = R.id.wallet_box_2;
-            }},
-            new ID(){{
-                WalletName = R.id.wallet_name_3;
-                WalletPrice = R.id.wallet_price_3;
-                Wallet = R.id.wallet_3;
-                WalletBox = R.id.wallet_box_3;
-            }},
-            new ID(){{
-                WalletName = R.id.wallet_name_4;
-                WalletPrice = R.id.wallet_price_4;
-                Wallet = R.id.wallet_4;
-                WalletBox = R.id.wallet_box_4;
-            }},
-            new ID(){{
-                WalletName = R.id.wallet_name_5;
-                WalletPrice = R.id.wallet_price_5;
-                Wallet = R.id.wallet_5;
-                WalletBox = R.id.wallet_box_5;
-            }},
-    };
+
+    public static ID[] ids = new ID[] { new ID() {
+        {
+            WalletName = R.id.wallet_name_1;
+            WalletPrice = R.id.wallet_price_1;
+            Wallet = R.id.wallet_1;
+            WalletBox = R.id.wallet_box_1;
+        }
+    }, new ID() {
+        {
+            WalletName = R.id.wallet_name_2;
+            WalletPrice = R.id.wallet_price_2;
+            Wallet = R.id.wallet_2;
+            WalletBox = R.id.wallet_box_2;
+        }
+    }, new ID() {
+        {
+            WalletName = R.id.wallet_name_3;
+            WalletPrice = R.id.wallet_price_3;
+            Wallet = R.id.wallet_3;
+            WalletBox = R.id.wallet_box_3;
+        }
+    }, new ID() {
+        {
+            WalletName = R.id.wallet_name_4;
+            WalletPrice = R.id.wallet_price_4;
+            Wallet = R.id.wallet_4;
+            WalletBox = R.id.wallet_box_4;
+        }
+    }, new ID() {
+        {
+            WalletName = R.id.wallet_name_5;
+            WalletPrice = R.id.wallet_price_5;
+            Wallet = R.id.wallet_5;
+            WalletBox = R.id.wallet_box_5;
+        }
+    }, };
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i(TAG, "on receive event");
@@ -79,50 +84,45 @@ public class NewAppWidget extends AppWidgetProvider {
             int[] ids = intent.getExtras().getIntArray(WIDGET_IDS_KEY);
             if (intent.hasExtra(WIDGET_DATA_KEY)) {
                 Object data = intent.getExtras().getParcelable(WIDGET_DATA_KEY);
-//                this.update(context, AppWidgetManager.getInstance(context), ids, data);
+                // this.update(context, AppWidgetManager.getInstance(context), ids, data);
             } else {
                 this.onUpdate(context, AppWidgetManager.getInstance(context), ids);
             }
-        } else super.onReceive(context, intent);
+        } else
+            super.onReceive(context, intent);
     }
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId, Wallet[] wallets) {
-
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Wallet[] wallets) {
 
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-        for (int i = 0; i <ids.length; i++) {
+        for (int i = 0; i < ids.length; i++) {
             if (wallets.length > i) {
                 views.setViewVisibility(ids[i].Wallet, View.VISIBLE);
                 views.setTextViewText(ids[i].WalletName, wallets[i].Name);
-                views.setInt(ids[i].WalletBox, "setBackgroundColor",
-                        Color.parseColor(wallets[i].Icon.Color)
-                );
+                views.setInt(ids[i].WalletBox, "setBackgroundColor", Color.parseColor(wallets[i].Icon.Color));
                 float price = 0;
-                for (TotalYear t :
-                        wallets[i].TotalPerYear) {
+                for (TotalYear t : wallets[i].TotalPerYear) {
                     price += t.Total;
                 }
-                views.setTextViewText(ids[i].WalletPrice, new DecimalFormat("#.##").format(price) + " " + wallets[i].Currency.Symbol);
+                views.setTextViewText(ids[i].WalletPrice,
+                        new DecimalFormat("#.##").format(price) + " " + wallets[i].Currency.Symbol);
             } else {
                 views.setViewVisibility(ids[i].Wallet, View.GONE);
             }
         }
 
-
         // Set on click button
         Log.i(TAG, "create activity");
         Intent intent = new Intent(context, MainActivity.class);
-        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);  // Identifies the particular widget...
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId); // Identifies the particular widget...
         intent.putExtra("module", "freeconomy");
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         // Make the pending intent unique...
         PendingIntent pendIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         Log.i(TAG, "set on click");
         views.setOnClickPendingIntent(R.id.mainview, pendIntent);
-
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -141,11 +141,10 @@ public class NewAppWidget extends AppWidgetProvider {
             Gson g = new Gson();
             try {
                 wallets = g.fromJson(impl, Wallet[].class);
-            } catch(Exception e) {
+                Log.i(TAG, "number wallets : " + wallets.length);
+            } catch (Exception e) {
                 Log.e(TAG, "fail to unmarshal " + e);
             }
-
-            Log.i(TAG, "number wallets : " + wallets.length);
         }
 
         Log.i(TAG, "update all widgets");
@@ -166,4 +165,3 @@ public class NewAppWidget extends AppWidgetProvider {
         // Enter relevant functionality for when the last widget is disabled
     }
 }
-
